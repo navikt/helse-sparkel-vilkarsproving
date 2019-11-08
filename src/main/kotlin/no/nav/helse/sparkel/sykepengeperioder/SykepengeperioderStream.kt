8e22a0853231b3281ab3b8f05bf822b05b32007a
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-11-08 15:41:29 +0100

    Fjerner midlertidig fiks for å håndtere gamle behov

--- src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/SykepengeperioderStream.kt
+++ src/main/kotlin/no/nav/helse/sparkel/sykepengeperioder/SykepengeperioderStream.kt
@@ -53,7 +53,7 @@ fun Application.sykepengeperioderApplication(): KafkaStreams {
 
     builder.stream<String, JsonNode>(
             listOf(behovTopic), Consumed.with(Serdes.String(), JsonNodeSerde(objectMapper))
-            .withOffsetResetPolicy(Topology.AutoOffsetReset.EARLIEST)
+            .withOffsetResetPolicy(Topology.AutoOffsetReset.LATEST)
     ).peek { key, value ->
         log.info("mottok melding key=$key value=$value")
     }.filter { _, value ->
@@ -61,12 +61,12 @@ fun Application.sykepengeperioderApplication(): KafkaStreams {
     }.filterNot { _, value ->
         value.harLøsning()
     }.filter { _, value ->
-        value.has("aktørId")
+        value.has("aktørId") && value.has("tom")
     }.peek { key, value ->
         log.info("løser behov key=$key")
     }.mapValues { _, value ->
         try {
-            value.setLøsning(lagLøsning(spoleClient, value["aktørId"].textValue(), value["tom"]?.let { LocalDate.parse(it.textValue()) } ?: LocalDate.now()))
+            value.setLøsning(lagLøsning(spoleClient, value["aktørId"].textValue(), LocalDate.parse(value["tom"].textValue())))
         } catch (err: Exception) {
             log.error("feil ved henting av spole-data: ${err.message}", err)
             null
