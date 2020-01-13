adc6aeb0f78d22fa3c5d837823c2fba72800d770
Knut Nygaard <knut.nygaard@nav.no>
2020-01-13 09:42:50 +0100

    Fjerna noe sensitiv logg. Filterer bort behov uten nøkkel
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- src/main/kotlin/no/nav/helse/sparkel/VilkårsStream.kt
+++ src/main/kotlin/no/nav/helse/sparkel/VilkårsStream.kt
@@ -1,54 +1,61 @@
 package no.nav.helse.sparkel
 
-import com.fasterxml.jackson.databind.*
-import com.fasterxml.jackson.databind.node.*
-import no.nav.helse.sparkel.serde.*
-import no.nav.tjeneste.pip.egen.ansatt.v1.*
-import org.apache.kafka.clients.*
-import org.apache.kafka.common.config.*
-import org.apache.kafka.common.serialization.*
-import org.apache.kafka.streams.*
-import org.apache.kafka.streams.errors.*
-import org.apache.kafka.streams.kstream.*
-import java.io.*
-import java.time.*
-import java.util.*
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.BooleanNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.fasterxml.jackson.databind.node.ObjectNode
+import net.logstash.logback.argument.StructuredArguments.keyValue
+import no.nav.helse.sparkel.serde.JsonNodeSerde
+import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1
+import no.nav.tjeneste.pip.egen.ansatt.v1.WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.common.config.SaslConfigs
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.errors.LogAndFailExceptionHandler
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.Produced
+import java.io.File
+import java.time.Duration
+import java.util.Properties
 
 private const val behovstype = "EgenAnsatt"
 private const val behovTopic = "privat-helse-sykepenger-behov"
 
 fun startStream(
-        egenAnsattService: EgenAnsattV1,
-        environment: Environment,
-        streamsConfig: Properties = streamsConfig(environment),
-        offsetResetPolicy: Topology.AutoOffsetReset = Topology.AutoOffsetReset.LATEST,
-        liveness: Liveness
+    egenAnsattService: EgenAnsattV1,
+    environment: Environment,
+    streamsConfig: Properties = streamsConfig(environment),
+    offsetResetPolicy: Topology.AutoOffsetReset = Topology.AutoOffsetReset.LATEST,
+    liveness: Liveness
 ): KafkaStreams {
     val builder = StreamsBuilder()
 
     builder.stream<String, JsonNode>(
-            listOf(behovTopic), Consumed.with(Serdes.String(), JsonNodeSerde(objectMapper))
+        listOf(behovTopic), Consumed.with(Serdes.String(), JsonNodeSerde(objectMapper))
             .withOffsetResetPolicy(offsetResetPolicy)
-    ).peek { key, value ->
-        log.info("mottok melding key=$key value=$value")
-    }.filter { _, value ->
-        value.skalOppfyllesAvOss(behovstype)
-    }.filterNot { _, value ->
-        value.harLøsning()
-    }.filter { _, value ->
-        value.hasNonNull("fødselsnummer")
-    }.mapValues { _, value ->
-        val fnr = value["fødselsnummer"].textValue()
-        val erEgenAnsatt = egenAnsattService.erEgenAnsatt(fnr)
-        value.setLøsning(
+    )
+        .peek { key, _ -> log.info("mottok melding {}", keyValue("behovId", key)) }
+        .filter { _, value -> value.skalOppfyllesAvOss(behovstype) }
+        .filterNot { _, value -> value.harLøsning() }.filter { _, value ->
+            value.hasNonNull("fødselsnummer")
+        }
+        .mapValues { _, value ->
+            val fnr = value["fødselsnummer"].textValue()
+            val erEgenAnsatt = egenAnsattService.erEgenAnsatt(fnr)
+            value.setLøsning(
                 ObjectNode(
-                        JsonNodeFactory.instance,
-                        mapOf(behovstype to BooleanNode.valueOf(erEgenAnsatt))
+                    JsonNodeFactory.instance,
+                    mapOf(behovstype to BooleanNode.valueOf(erEgenAnsatt))
                 )
-        )
-    }.peek { key, _ ->
-        log.info("løst behov for key=$key")
-    }.to(behovTopic, Produced.with(Serdes.String(), JsonNodeSerde(objectMapper)))
+            )
+        }
+        .peek { key, _ -> log.info("løst behov for key=$key") }
+        .to(behovTopic, Produced.with(Serdes.String(), JsonNodeSerde(objectMapper)))
 
     return KafkaStreams(builder.build(), streamsConfig).apply {
         setStateListener { newState, _ ->
@@ -63,21 +70,12 @@ fun startStream(
 }
 
 private fun EgenAnsattV1.erEgenAnsatt(fnr: String) =
-        hentErEgenAnsattEllerIFamilieMedEgenAnsatt(WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest().withIdent(fnr))
-                .isEgenAnsatt
+    hentErEgenAnsattEllerIFamilieMedEgenAnsatt(WSHentErEgenAnsattEllerIFamilieMedEgenAnsattRequest().withIdent(fnr))
+        .isEgenAnsatt
 
-private fun JsonNode.skalOppfyllesAvOss(type: String) =
-        this["@behov"]?.let {
-            if (it.isArray) {
-                it.map { b -> b.asText() }.any { t -> t == type }
-            } else it.asText() == type
-        } ?: false
-
-private fun JsonNode.harLøsning() =
-        hasNonNull("@løsning")
-
-private fun JsonNode.setLøsning(løsning: JsonNode) =
-        (this as ObjectNode).set("@løsning", løsning)
+private fun JsonNode.skalOppfyllesAvOss(type: String) = this["@behov"].map(JsonNode::asText).any { it == type }
+private fun JsonNode.harLøsning() = hasNonNull("@løsning")
+private fun JsonNode.setLøsning(løsning: JsonNode) = (this as ObjectNode).set("@løsning", løsning)
 
 private fun streamsConfig(environment: Environment) = Properties().apply {
     put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, environment.kafkaBootstrapServers)
@@ -89,8 +87,8 @@ private fun streamsConfig(environment: Environment) = Properties().apply {
     put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT")
 
     put(
-            SaslConfigs.SASL_JAAS_CONFIG,
-            "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${environment.serviceUser.username}\" password=\"${environment.serviceUser.password}\";"
+        SaslConfigs.SASL_JAAS_CONFIG,
+        "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${environment.serviceUser.username}\" password=\"${environment.serviceUser.password}\";"
     )
 
     try {
