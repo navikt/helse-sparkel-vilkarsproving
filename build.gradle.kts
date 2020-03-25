3b3e1ce7118c43353dd1430af03418b729bbaddc
David Steinsland <david@steinsland.email>
2020-03-25 20:31:18 +0100

    oppdater rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ buildscript {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.1822995")
+    implementation("com.github.navikt:rapids-and-rivers:1.39bea05")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
 
