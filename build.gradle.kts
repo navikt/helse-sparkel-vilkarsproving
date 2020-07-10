7fe04aa2ced4042f5c1c3c259a56b713f0c3db9a
Martin Solheim <martin.solheim@me.com>
2020-07-10 12:20:25 +0200

    Bumper dependencies og gradle versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val ktorVersion = "1.3.2"
-val cxfVersion = "3.3.4"
-val egenAnsattVersion = "1.2019.09.25-00.21-49b69f0625e0"
-val jaxWsVersion = "2.3.2"
-val wireMockVersion = "2.19.0"
-val mockkVersion = "1.9.3"
-val junitJupiterVersion = "5.6.0"
-val mainClass = "no.nav.helse.sparkel.AppKt"
+val cxfVersion = "3.3.7"
+val junitJupiterVersion = "5.6.2"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -19,88 +12,71 @@ buildscript {
     }
 }
 
+repositories {
+    mavenCentral()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://jitpack.io")
+    maven("https://packages.confluent.io/maven/")
+}
+
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
 
-    implementation("com.sun.xml.ws:jaxws-ri:$jaxWsVersion")
+    implementation("com.sun.xml.ws:jaxws-ri:2.3.3")
     implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
     implementation("javax.activation:activation:1.1.1")
 
-    implementation("no.nav.tjenestespesifikasjoner:egenansatt-v1-tjenestespesifikasjon:$egenAnsattVersion")
+    implementation("no.nav.tjenestespesifikasjoner:egenansatt-v1-tjenestespesifikasjon:1.2019.09.25-00.21-49b69f0625e0")
 
-    testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("com.github.tomakehurst:wiremock:$wireMockVersion") {
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("com.github.tomakehurst:wiremock:2.27.1") {
         exclude(group = "junit")
     }
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
-    testImplementation("no.nav:kafka-embedded-env:2.2.3")
-    testImplementation("org.awaitility:awaitility:3.1.6")
+    testImplementation("no.nav:kafka-embedded-env:2.4.0")
+    testImplementation("org.awaitility:awaitility:4.0.3")
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
 }
 
-val githubUser: String by project
-val githubPassword: String by project
-
-repositories {
-    mavenCentral()
-    maven("https://kotlin.bintray.com/ktor")
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
     }
-    maven("https://packages.confluent.io/maven/")
-}
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
-}
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
 
-tasks.named<Jar>("jar") {
-    archiveBaseName.set("app")
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.sparkel.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
 
-    manifest {
-        attributes["Main-Class"] = mainClass
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-            it.name
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
 
-    doLast {
-        configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
-            if (!file.exists())
-                it.copyTo(file)
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
         }
     }
-}
-
-tasks.named<KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "6.0.1"
 }
