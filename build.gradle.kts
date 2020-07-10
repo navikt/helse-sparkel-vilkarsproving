ffa13f7c5f0cbd9c0720029b89a4a73b2f83d474
Martin Solheim <martin.solheim@me.com>
2020-07-10 16:04:44 +0200

    Bumper rapids and rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
+    implementation("com.github.navikt:rapids-and-rivers:fa839faa1c")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
 
